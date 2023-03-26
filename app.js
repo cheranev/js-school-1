c5a66b5d0149eaa164841aaf2c17d98497662705
cheranev <128130986+cheranev@users.noreply.github.com>
2023-03-26 23:48:09 +0300

    upload driver-6

---
+++ app.js
@@ -0,0 +1,5 @@
+const hasLicense = true;
+const ageDriver = 18;
+const isDrunk = false;
+
+console.log(hasLicense == !false && ageDriver >= 18 && isDrunk == !true && "He can drive" || "He can`t drive");
