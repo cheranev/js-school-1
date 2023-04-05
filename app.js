8b48bd76839bb9c406edb3ed72f71809e10a3b94
cheranev <128130986+cheranev@users.noreply.github.com>
2023-04-05 10:54:43 +0300

    fix and remove "=" operators

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const hasLicense = true;
 const ageDriver = 18;
-const isDrunk = false;
+const isNotDrunk = true;
 
-console.log(hasLicense == !false && ageDriver >= 18 && isDrunk == !true && "He can drive" || "He can`t drive");
+console.log(hasLicense && ageDriver >= 18 && isNotDrunk && "He can drive" || "He can`t drive");
