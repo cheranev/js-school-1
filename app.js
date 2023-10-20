0ff0ffb962e4eadb88e15f67e506b5c878bdf0a6
rcheranev <rcheranev@rvision.ru>
2023-10-20 16:56:27 +0300

    upload new todolist version

--- app.js
+++ app.js
@@ -1,81 +1,46 @@
 let todoItems = [];
 
-function addTask(id, title, priority) {
-  const todo = {
-    id,
-    title,
-    priority,
+function toDoListObject() {
+  let objectListToDo = {
+    title: this.title,
+    id: this.id,
+    priority: this.priority,
   };
+  function createNewTask(obTitle, obId, obPriority) {
+    objectListToDo.title = obTitle;
+    objectListToDo.id = obId;
+    objectListToDo.priority = obPriority;
+    todoItems.push(objectListToDo);
+    objectListToDo = {};
+  }
+  function removeTaskById(deleteId) {
+    const indexOfObject = todoItems.findIndex((object) => {
+      return object.id === deleteId;
+    });
+    todoItems.splice(indexOfObject, 1);
+  }
+  function updateTitleAndPriority(idToUpdate, newTitle, newPriority) {
+    let itemIndex = todoItems.findIndex((x) => x.id == idToUpdate);
 
-  todoItems.push(todo);
-  console.log(
-    `Task with id "${id}"  and title "${title}" is succesfully added to ur ToDoList`
-  );
+    let item = todoItems[itemIndex];
+    item.title = newTitle;
+    item.priority = newPriority;
+
+    todoItems[itemIndex] = item;
+  }
+  function sortToDoList() {
+    todoItems.sort((a, b) => {
+      return a.priority - b.priority;
+    });
+  }
+  createNewTask("Task number 3", 3, 123);
+  createNewTask("Task number 1", 1, 12);
+  createNewTask("Task number 2", 2, 14);
+  createNewTask("Task number 3", 3, 15);
+  removeTaskById(1);
+  updateTitleAndPriority(2, "New Task name", 3);
+  sortToDoList();
 }
-function removeTask(id, todoList) {
-  let i = 0;
-  for (i in todoList) {
-    if (Object.values(todoList)[i]["id"] == id) {
-      delete todoList[i];
-      console.log(`Task with id "${id}"  succesfully removed from ToDoList`);
-      return;
-    } else {
-      continue;
-    }
-  }
+toDoListObject();
 
-  todoItems.filter((element) => {
-    if (Object.keys(element).length !== 0) {
-      return true;
-    }
-
-    return false;
-  });
-}
-
-function changeNameOrPriorityTask(id, newTaskName, newPriority, todoList) {
-  switch (newTaskName) {
-    case null:
-      break;
-    default:
-      for (i in todoList) {
-        if (Object.values(todoList)[i]["id"] == id) {
-          todoList[i]["title"] = newTaskName;
-          todoList[i]["priority"] = newPriority;
-          return;
-        } else {
-        }
-      };
-  }
-}
-
-// create more task
-addTask(57, "asdasd", 65);
-addTask(2, "toremove", 2);
-addTask(1, "qweqwe", 3);
-addTask(123, "dfgdfg", 2);
-// remove task and remove empty objects
-removeTask(57, todoItems);
-const results = todoItems.filter((element) => {
-  if (Object.keys(element).length !== 0) {
-    return true;
-  }
-
-  return false;
-});
-// change name or priority by id
-changeNameOrPriorityTask(1, "newTaskName", 12, results);
-
-function sortToDoList(a, b) {
-  if (a.id < b.id) {
-    return -1;
-  }
-  if (a.id > b.id) {
-    return 1;
-  }
-  return 0;
-}
-// finally, sort todolist
-results.sort(sortToDoList);
-
-console.log(results);
+console.log(todoItems);
