c087d47de3198d071e19bc4602b5492517e824fe
rcheranev <rcheranev@rvision.ru>
2023-10-23 11:32:58 +0300

    implementation of objects and methods for it

--- app.js
+++ app.js
@@ -1,46 +1,50 @@
-let todoItems = [];
-
-function toDoListObject() {
-  let objectListToDo = {
-    title: this.title,
-    id: this.id,
-    priority: this.priority,
-  };
-  function createNewTask(obTitle, obId, obPriority) {
-    objectListToDo.title = obTitle;
-    objectListToDo.id = obId;
-    objectListToDo.priority = obPriority;
-    todoItems.push(objectListToDo);
-    objectListToDo = {};
-  }
-  function removeTaskById(deleteId) {
-    const indexOfObject = todoItems.findIndex((object) => {
+const toDoList = {
+  list: [],
+  createNewTask: function (newItems) {
+    this.list.push(newItems);
+  },
+  removeTaskById: function (deleteId) {
+    const indexOfObject = this.list.findIndex((object) => {
       return object.id === deleteId;
     });
-    todoItems.splice(indexOfObject, 1);
-  }
-  function updateTitleAndPriority(idToUpdate, newTitle, newPriority) {
-    let itemIndex = todoItems.findIndex((x) => x.id == idToUpdate);
+    if (indexOfObject !== null) {
+      this.list.splice(indexOfObject, 1);
+    }
+  },
+  updateTitleAndPriority: function (idToUpdate, newTitle, newPriority) {
+    let itemIndex = this.list.findIndex((x) => x.id == idToUpdate);
 
-    let item = todoItems[itemIndex];
+    let item = this.list[itemIndex];
     item.title = newTitle;
     item.priority = newPriority;
 
-    todoItems[itemIndex] = item;
-  }
-  function sortToDoList() {
-    todoItems.sort((a, b) => {
+    this.list[itemIndex] = item;
+  },
+  sortToDoList: function () {
+    this.list.sort((a, b) => {
       return a.priority - b.priority;
     });
-  }
-  createNewTask("Task number 3", 3, 123);
-  createNewTask("Task number 1", 1, 12);
-  createNewTask("Task number 2", 2, 14);
-  createNewTask("Task number 3", 3, 15);
-  removeTaskById(1);
-  updateTitleAndPriority(2, "New Task name", 3);
-  sortToDoList();
-}
-toDoListObject();
+  },
+};
+toDoList.createNewTask({
+  title: 'Выкинуть мусор',
+  id: 1,
+  priority: 2,
+});
 
-console.log(todoItems);
+toDoList.createNewTask({
+  title: 'Погулять',
+  id: 2,
+  priority: 3,
+});
+
+toDoList.createNewTask({
+  title: 'Помыть посуду',
+  id: 3,
+  priority: 1,
+});
+
+toDoList.removeTaskById(3);
+toDoList.sortToDoList();
+toDoList.updateTitleAndPriority(1, 'title', 'Сделать уроки');
+console.log(toDoList.list);
