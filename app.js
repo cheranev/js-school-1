5d746a765fabe1e3091fe54b29ff0f7be1ef81b1
rcheranev <rcheranev@rvision.ru>
2023-10-23 14:17:02 +0300

    fix defect

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ const toDoList = {
     }
   },
   updateTitleAndPriority: function (idToUpdate, newTitle, newPriority) {
-    let itemIndex = this.list.findIndex((x) => x.id == idToUpdate);
+    const itemIndex = this.list.findIndex((x) => x.id == idToUpdate);
 
-    let item = this.list[itemIndex];
-    item.title = newTitle;
-    item.priority = newPriority;
+    if (itemIndex === -1) {
+      throw new Error("Invalid index for update value");
+    } else {
+      let item = this.list[itemIndex];
+      item.title = newTitle;
+      item.priority = newPriority;
 
-    this.list[itemIndex] = item;
+      this.list[itemIndex] = item;
+    }
   },
   sortToDoList: function () {
     this.list.sort((a, b) => {
@@ -27,24 +31,24 @@ const toDoList = {
   },
 };
 toDoList.createNewTask({
-  title: 'Выкинуть мусор',
+  title: "Выкинуть мусор",
   id: 1,
   priority: 2,
 });
 
 toDoList.createNewTask({
-  title: 'Погулять',
+  title: "Погулять",
   id: 2,
   priority: 3,
 });
 
 toDoList.createNewTask({
-  title: 'Помыть посуду',
+  title: "Помыть посуду",
   id: 3,
   priority: 1,
 });
 
 toDoList.removeTaskById(3);
 toDoList.sortToDoList();
-toDoList.updateTitleAndPriority(1, 'title', 'Сделать уроки');
+toDoList.updateTitleAndPriority(1, "title", "Сделать уроки");
 console.log(toDoList.list);
